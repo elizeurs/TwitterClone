661d1e6eb730941dee166cf302bec1b731493539
Elizeu RS <13ders@gmail.com>
2023-07-02 17:23:19 -0300

    add twitter logo and profile button to navigationBar | refactor action buttons

---
+++ TwitterClone/TwitterClone/Controllers/General/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  TwitterClone
+//
+//  Created by Elizeu RS on 02/07/23.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+    navigationItem.title = "Profile"
+  }
+}
