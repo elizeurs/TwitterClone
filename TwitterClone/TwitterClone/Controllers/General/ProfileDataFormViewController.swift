9a934950b5573f7adc7bdb8b102deedaa9ac15e1
Elizeu RS <13ders@gmail.com>
2023-07-10 17:33:26 -0300

    part 11 - finish logging users in, handle errors and sign out

---
+++ TwitterClone/TwitterClone/Controllers/General/ProfileDataFormViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileDataFormViewController.swift
+//  TwitterClone
+//
+//  Created by Elizeu RS on 10/07/23.
+//
+
+import UIKit
+
+class ProfileDataFormViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+}
--- TwitterClone/TwitterClone/ViewModels/AuthenticationViewModel.swift
+++ TwitterClone/TwitterClone/ViewModels/AuthenticationViewModel.swift
@@ -39,6 +39,9 @@ final class AuthenticationViewModel: ObservableObject {
     guard let email = email,
     let password = password else { return }
     AuthManager.shared.registerUser(with: email, password: password)
+      .handleEvents(receiveOutput: { [weak self] user in
+        self?.user = user
+      })
       .sink { [weak self] completion in
         
         if case .failure(let error) = completion {
@@ -46,9 +49,22 @@ final class AuthenticationViewModel: ObservableObject {
         }
         
       } receiveValue: { [weak self] user in
-        self?.user = user
+        self?.createRecord(for: user)
       }
       .store(in: &subscriptions)
+  }
+  
+  func createRecord(for user: User) {
+    DatabaseManager.shared.collectionUsers(add: user)
+      .sink { [weak self] completion in
+        if case .failure(let error) = completion {
+          self?.error = error.localizedDescription
+        }
+    } receiveValue: { state in
+      print("Adding user record to database: \(state)")
+    }
+    .store(in: &subscriptions)
+
   }
   
   func loginUser() {
